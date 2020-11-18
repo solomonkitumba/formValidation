0cb83dcfe1ef76ea1005f87fe5a431a3e1d4be4d
Swipe Global inc <swipeglobalinc@gmail.com>
2020-11-18 15:01:24 +0300

    first release

---
+++ app.js
@@ -0,0 +1,64 @@
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+const email = document.getElementById('email');
+
+//shoeinput error message
+function showError(input, message) {
+    const formControl = input.parentElement;
+    formControl.className = 'formcontrol error';
+    const small = form.querySelector('small');
+    small.innerText = message;
+}
+
+//show success outline
+
+function showSuccess(input) {
+    const formControl = input.parentElement;
+    formControl.className = 'formcontrol success';
+}
+
+//validate email
+
+function isValidEmail(email) {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+//event Listeners
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    console.log(username.value);
+
+    if (username.value === '') {
+        showError(username, "Username is Required");
+
+    } else {
+        showSuccess(username);
+    }
+
+    if (email.value === '') {
+        showError(email, 'Email is Required');
+
+    } else if (!isValidEmail(email.value)) {
+        showError(email, "Email is not valid");
+
+    } else {
+        showSuccess(email);
+    }
+
+    if (password.value === '') {
+        showError(password, "Password is Required");
+
+    } else {
+        showSuccess(password);
+    }
+
+    if (password2.value === '') {
+        showError(password2, "Comfirm is Required");
+
+    } else {
+        showSuccess(password2);
+    }
+
+});
